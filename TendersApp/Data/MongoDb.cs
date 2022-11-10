724b1ed704892ae61ab67b97f2d96ecfb264d05b
bulatello28 <bulatalmazovich.99@mail.ru>
2022-11-10 15:09:57 +0300

    Добавьте файлы проекта.

---
+++ TendersApp/Data/MongoDb.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace TendersApp.Data
+{
+    public class MongoDb
+    {
+        public static void AddUserToDataBase(User user)
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("Users");
+            var collection = database.GetCollection<User>("UserCollection");
+            collection.InsertOne(user);
+        }
+
+        public static void ReplaceOneInDataBase(User user)
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("Users");
+            var filter = new BsonDocument("_id", user._id);
+            var collection = database.GetCollection<User>("UserCollection");
+            collection.ReplaceOne(filter, user);
+        }
+    }
+}
---
+++ TendersApp/Users/User.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace TendersApp.Users
+{
+    public class User
+    {
+        [BsonId]
+        public ObjectId _id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+    }
+}
